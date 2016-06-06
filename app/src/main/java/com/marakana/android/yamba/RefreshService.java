d26e73f7ffe36d21c2c3fabea5c00fccf6f6166a
Mark <ywtean@gmail.com>
2016-06-06 15:35:17 +0800

    Initial commit

---
+++ app/src/main/java/com/marakana/android/yamba/RefreshService.java
@@ -0,0 +1,66 @@
+package com.marakana.android.yamba;
+
+import java.util.List;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClient.Status;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
+public class RefreshService extends IntentService {
+	private static final String TAG = RefreshService.class.getSimpleName();
+
+	public RefreshService() {
+		super(TAG);
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Log.d(TAG, "onCreated");
+	}
+
+	// Executes on a worker thread
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		String username = prefs.getString("username", "student");
+		String password = prefs.getString("password", "password");
+
+		// Check that username and password are not empty
+		if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+			Toast.makeText(this, "Please update your username and password",
+					Toast.LENGTH_LONG).show();
+			return;
+		}
+		Log.d(TAG, "onStarted");
+		YambaClient cloud = new YambaClient(username, password,"http://yamba.newcircle.com/api");
+		try {
+			List<Status> timeline = cloud.getTimeline(30);
+			Log.d(TAG, String.valueOf(timeline.size()));
+			for (Status status : timeline) {
+				Log.d(TAG,
+						String.format("%s: %s", status.getUser(),
+								status.getMessage()));
+			}
+		} catch (YambaClientException e) {
+			Log.e(TAG, "Failed to fetch the timeline", e);
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroyed");
+	}
+
+}
